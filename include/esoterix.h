302fde125ec3299ab803095eab207bc98167a55c
mbecel <mael1.becel@epitech.eu>
2022-06-11 20:30:46 +0200

    🏗️  (src/, include/): add basic starter files

---
+++ include/esoterix.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** esoterix.c
+** File description:
+** esoterix
+*/
+
+#ifndef ESOTERIX
+    #define ESOTERIX
+    #include "my.h"
+    #include "printf.h"
+    #include "formatstring.h"
+
+    #include <stdbool.h>
+    #include <stddef.h>
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** main
+*/
+
+#include "esoterix.h"
+
+int main (UNUSED int ac, UNUSED char **argv, UNUSED char **env)
+{
+    return 0;
+}
